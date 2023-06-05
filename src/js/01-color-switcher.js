36255f02f17b6a572f0cff34b37c160769e212ca
amosovandrey <rivercity.dp@gmail.com>
2023-06-05 16:01:13 +0300

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBodyColor() {
+  document.querySelector('body').style.backgroundColor = getRandomHexColor();
+}
+
+const DELAY = 1000;
+let timerID = null;
+
+function onStartClick() {
+  timerID = setInterval(changeBodyColor, DELAY);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopClick() {
+  clearInterval(timerID);
+  startBtn.disabled = false;
+}
