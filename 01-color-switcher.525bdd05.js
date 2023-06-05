5d6bc0598f3fd6b432fb8e26a5ef7518da9171c0
amosovandrey <121553573+amosovandrey@users.noreply.github.com>
2023-06-05 13:01:43 +0000

    Deploying to gh-pages from @ amosovandrey/goit-js-hw-09@36255f02f17b6a572f0cff34b37c160769e212ca 🚀

---
+++ 01-color-switcher.525bdd05.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function a(){document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){n=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1})),e.disabled=!0;var n=null}();
+//# sourceMappingURL=01-color-switcher.525bdd05.js.map
