1b6059a09f0abf6243c1c9d6c4b6b60d1ef91637
amosovandrey <rivercity.dp@gmail.com>
2023-06-06 17:46:52 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('form');
+console.log(formEl);
+
+formEl.addEventListener('click', onFormClick);
+
+function onFormClick(e) {
+  if (e.target === formEl.querySelector('button')) {
+    e.preventDefault();
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
