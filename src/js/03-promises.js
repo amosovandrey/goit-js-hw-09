e915ece92bfe5ea67d604f0c2e132658c9edf5b5
amosovandrey <rivercity.dp@gmail.com>
2023-06-11 16:14:29 +0300

    task 03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,62 @@
 import Notiflix from 'notiflix';
 
 const formEl = document.querySelector('form');
-console.log(formEl);
 
-formEl.addEventListener('click', onFormClick);
+const delayEl = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
 
-function onFormClick(e) {
-  if (e.target === formEl.querySelector('button')) {
-    e.preventDefault();
+let intervalID = null;
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const delay = delayEl.value;
+  const step = stepEl.value;
+  const amount = amountEl.value;
+
+  let createdPromises = 0;
+
+  let position = 0;
+
+  if (intervalID) {
+    clearInterval(intervalID);
   }
+
+  intervalID = setInterval(() => {
+    position += 1;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    createdPromises += 1;
+
+    if (createdPromises >= amount) {
+      clearInterval(intervalID);
+      return;
+    }
+  }, step);
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
