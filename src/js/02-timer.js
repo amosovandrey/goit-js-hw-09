58df1ffb30bd5df27ad31e99aeeefd293b1f2bf1
amosovandrey <rivercity.dp@gmail.com>
2023-06-12 17:27:18 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -18,12 +18,14 @@ const options = {
       });
     } else {
       startBtn.disabled = false;
+      datetimePicker.disabled = false;
     }
   },
 };
 
 const startBtn = document.querySelector('[data-start]');
 const datetimePicker = document.getElementById('datetime-picker');
+
 const daysValue = document.querySelector('[data-days]');
 const hoursValue = document.querySelector('[data-hours]');
 const minutesValue = document.querySelector('[data-minutes]');
@@ -40,6 +42,7 @@ startBtn.addEventListener('click', startCountdown);
 
 function startCountdown() {
   startBtn.disabled = true;
+  datetimePicker.disabled = true;
   const selectedDate = calendar.parseDate(datetimePicker.value);
   // let countTime = selectedDate.getTime() - Date.now();
 
@@ -86,91 +89,3 @@ function updateCountdownValues(countTime) {
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
-
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-// import Notiflix from 'notiflix';
-
-// const datetimePicker = document.getElementById('datetime-picker');
-// const startBtn = document.querySelector('[data-start]');
-// const daysValue = document.querySelector('[data-days]');
-// const hoursValue = document.querySelector('[data-hours]');
-// const minutesValue = document.querySelector('[data-minutes]');
-// const secondsValue = document.querySelector('[data-seconds]');
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     const selectedDate = selectedDates[0];
-
-//     if (selectedDate < Date.now()) {
-//       Notiflix.Notify.warning('Please choose a date in the future');
-//       startBtn.disabled = true;
-//     } else {
-//       startBtn.disabled = false;
-//     }
-//   },
-// };
-
-// flatpickr(datetimePicker, options);
-
-// let countdownInterval;
-
-// startBtn.addEventListener('click', () => {
-//   startCountdown();
-//   startBtn.disabled = true;
-// });
-
-// function startCountdown() {
-//   const selectedDate = flatpickr.parseDate(datetimePicker.value);
-//   const countdown = selectedDate.getTime() - Date.now();
-
-//   if (countdown <= 0) {
-//     updateTimerValues(0);
-//     Notiflix.Notify.success('Countdown finished!');
-//     return;
-//   }
-
-//   updateTimerValues(countdown);
-//   countdownInterval = setInterval(() => {
-//     const updatedCountdown = selectedDate.getTime() - Date.now();
-
-//     if (updatedCountdown <= 0) {
-//       clearInterval(countdownInterval);
-//       updateTimerValues(0);
-//       Notiflix.Notify.success('Countdown finished!');
-//       return;
-//     }
-
-//     updateTimerValues(updatedCountdown);
-//   }, 1000);
-// }
-
-// function updateTimerValues(countdown) {
-//   const { days, hours, minutes, seconds } = convertMs(countdown);
-//   daysValue.textContent = addLeadingZero(days);
-//   hoursValue.textContent = addLeadingZero(hours);
-//   minutesValue.textContent = addLeadingZero(minutes);
-//   secondsValue.textContent = addLeadingZero(seconds);
-// }
-
-// function convertMs(ms) {
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   const days = Math.floor(ms / day);
-//   const hours = Math.floor((ms % day) / hour);
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//   return { days, hours, minutes, seconds };
-// }
-
-// function addLeadingZero(value) {
-//   return value.toString().padStart(2, '0');
-// }
